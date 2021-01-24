6b6904f4cba936a7691f2825085566b8e3b911b3
liptoner <liptoner.mbox@gmail.com>
2021-01-24 19:03:51 +0100

    TMP

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,6 +2,7 @@
 const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
+
 const RESOURCES = {
   "version.json": "2a8de261a84fd7b19a0f4902850266e2",
 "manifest.json": "3aa090c891bfde34f0c4701768829ce2",
@@ -9,30 +10,30 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "d00e546d64b0ddb5dbf38b1f279949df",
-"/": "d00e546d64b0ddb5dbf38b1f279949df",
-"assets/NOTICES": "6052b20f6e35414246a8eb8c71f1a214",
-"assets/packages/progress_dialog/assets/double_ring_loading_io.gif": "e5b006904226dc824fdb6b8027f7d930",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "d21f791b837673851dd14f7c132ef32e",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3ca122272cfac33efb09d0717efde2fa",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "bdd8d75eb9e6832ccd3117e06c51f0d3",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b14fcf3ee94e3ace300b192e9e7c8c5d",
-"assets/AssetManifest.json": "396fdd4fd4f2a04eeb21a96eab8752d9",
-"assets/FontManifest.json": "ab9ee27749f78fe939561327fd324e25",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/assets/images/default.png": "f1d6836d93dc4af94ebe2c76e286f75d",
-"assets/assets/images/search.svg": "44b2ff5217efd3059f848d485599bf6e",
-"assets/assets/images/header_decoration.png": "a57e817e33a29b996813ec85d29065f0",
-"assets/assets/images/app_icon_1024.png": "6a8c20de4a3cc3368059348c5ac6ac2b",
-"assets/assets/images/welcome.jpg": "03b00aa35bc6a2fbb3d94c55f5f6eb9c",
-"assets/assets/fonts/SF-Pro-Text-Regular.otf": "404e4373cba1344d28a4a257152ac8b8",
-"assets/assets/free_decks/deck1/image_03.jpg": "055e6e96ec934b93037dc5037f4cdad2",
-"assets/assets/free_decks/deck1/image_02.jpg": "f6342d4e8aaa9e3073dbaeb190f89aa7",
-"assets/assets/free_decks/deck1/clap.mp3": "a9abeca268b3fe353cc33edb4486339f",
-"assets/assets/free_decks/deck1/image_01.png": "11e0115482c2152e0d5d189d05779df0",
-"assets/assets/free_decks/deck1/zupa.m4a": "1ee0053b9d9e888c9ed3f431080a05ce",
-"assets/assets/free_decks/deck1/image_04.jpg": "ccb0274837f8afa57d748ebe9cccfab2",
-"assets/assets/free_decks/deck1/tile.svg": "ad669fc9f4cdc428f813325764379a1f",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796"
+// "/": "d00e546d64b0ddb5dbf38b1f279949df",
+// "assets/NOTICES": "6052b20f6e35414246a8eb8c71f1a214",
+// "assets/packages/progress_dialog/assets/double_ring_loading_io.gif": "e5b006904226dc824fdb6b8027f7d930",
+// "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "d21f791b837673851dd14f7c132ef32e",
+// "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3ca122272cfac33efb09d0717efde2fa",
+// "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "bdd8d75eb9e6832ccd3117e06c51f0d3",
+// "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b14fcf3ee94e3ace300b192e9e7c8c5d",
+// "assets/AssetManifest.json": "396fdd4fd4f2a04eeb21a96eab8752d9",
+// "assets/FontManifest.json": "ab9ee27749f78fe939561327fd324e25",
+// "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
+// "assets/assets/images/default.png": "f1d6836d93dc4af94ebe2c76e286f75d",
+// "assets/assets/images/search.svg": "44b2ff5217efd3059f848d485599bf6e",
+// "assets/assets/images/header_decoration.png": "a57e817e33a29b996813ec85d29065f0",
+// "assets/assets/images/app_icon_1024.png": "6a8c20de4a3cc3368059348c5ac6ac2b",
+// "assets/assets/images/welcome.jpg": "03b00aa35bc6a2fbb3d94c55f5f6eb9c",
+// "assets/assets/fonts/SF-Pro-Text-Regular.otf": "404e4373cba1344d28a4a257152ac8b8",
+// "assets/assets/free_decks/deck1/image_03.jpg": "055e6e96ec934b93037dc5037f4cdad2",
+// "assets/assets/free_decks/deck1/image_02.jpg": "f6342d4e8aaa9e3073dbaeb190f89aa7",
+// "assets/assets/free_decks/deck1/clap.mp3": "a9abeca268b3fe353cc33edb4486339f",
+// "assets/assets/free_decks/deck1/image_01.png": "11e0115482c2152e0d5d189d05779df0",
+// "assets/assets/free_decks/deck1/zupa.m4a": "1ee0053b9d9e888c9ed3f431080a05ce",
+// "assets/assets/free_decks/deck1/image_04.jpg": "ccb0274837f8afa57d748ebe9cccfab2",
+// "assets/assets/free_decks/deck1/tile.svg": "ad669fc9f4cdc428f813325764379a1f",
+// "favicon.png": "5dcef449791fa27946b3d35ad8803796"
 };
 
 // The application shell files that are downloaded before a service worker can
