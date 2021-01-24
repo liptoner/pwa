f5a69e129b755ecf23deee69780b981a057c612a
liptoner <liptoner.mbox@gmail.com>
2021-01-24 14:59:34 +0100

    TMP

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -143,7 +143,7 @@ self.addEventListener("fetch", (event) => {
   // If the URL is not the RESOURCE list then return to signal that the
   // browser should take over.
   if (key.endsWith('.mp3') || key.endsWith('.m4a')) {
-  	event.request.url = evetn.request.url.replace('pwa/index.html#/', 'pwa/assets') 
+  	event.request.url = event.request.url.replace('pwa/index.html#/', 'pwa/assets') 
   }
   else if (!RESOURCES[key]) {
     return;
